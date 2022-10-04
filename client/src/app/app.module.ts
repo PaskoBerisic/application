36d33ac29c7feb5d09de23685c840af4a9657489
Paško Berišić <paberisi@vsite.hr>
2022-10-04 13:43:26 +0200

    angular material added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatTableModule} from '@angular/material/table';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { BookListComponent } from './components/books/book-list/book-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/books/book-list/book-list.component.ts
+++ client/src/app/components/books/book-list/book-list.component.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/app/core/services/book.service';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-  
+  displayedColumns: string[] = ['id', 'name', 'price', 'description', 'action'];
+  dataSource: any;
+
   Books:any = [];
   constructor(private bookService: BookService) { }
   ngOnInit(): void {
     this.bookService.GetAll().subscribe(res => {
       console.log(res)
       this.Books =res;
-    });    
+    });   
+    this.dataSource = this.Books; 
   }
   delete(id:any, i:any) {
     console.log(id);

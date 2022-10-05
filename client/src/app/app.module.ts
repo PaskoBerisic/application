c64669c01212dffa495ec2c4177817997805a6a0
Paško Berišić <Paki.berka@gmail.com>
2022-10-05 22:16:46 +0200

    filter table implemented and changed styles

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +13,7 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { BookListComponent } from './components/books/book-list/book-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
@@ -27,6 +31,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     MatFormFieldModule,
     MatInputModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/books/book-list/book-list.component.ts
+++ client/src/app/components/books/book-list/book-list.component.ts
@@ -1,25 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/app/core/services/book.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-book-list',
@@ -28,15 +9,20 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class BookListComponent implements OnInit  {
   displayedColumns: string[] = ['id', 'name', 'price', 'description', 'action'];
+  displayedColumns2: string[] = ['id', 'name', 'price', 'description', 'action'];
+
   dataSource: any = [];
   Books:any = [];
-
+  dataSource2: any = [];
+  
   constructor(private bookService: BookService) {}
   ngOnInit(): void {
     this.bookService.GetAll().subscribe(res => {
       console.log(res)
       this.Books =res;
       this.dataSource =res;
+
+      this.dataSource2 = new MatTableDataSource(this.Books);
     });   
   }
   
@@ -48,4 +34,10 @@ export class BookListComponent implements OnInit  {
       })
     }
   }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource2.filter = filterValue.trim().toLowerCase();
+  }
+
 }

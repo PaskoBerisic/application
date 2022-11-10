8d179b7efcf3c96da03fae21e9d94cc77c3472d3
Paško Berišić <Paki.berka@gmail.com>
2022-11-10 17:47:56 +0100

    jjj

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use('/api/productCategories', productCategoriesRoute);
 app.use('/api/general', generalRoute);
 app.use('/api/orders', ordersRoute);
 
-// PORT
+// PORT jjjj
 const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log('Listening on port ' + port);
